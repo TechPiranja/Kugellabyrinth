cc258943750097b1d849fe4880b1dce2853d5f17
Piranja <techpiranja@gmail.com>
2021-06-09 11:17:32 +0200

    re-added toolbar

--- app/src/main/java/com/example/kugellabyrinth/MainActivity.java
+++ app/src/main/java/com/example/kugellabyrinth/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.kugellabyrinth;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override

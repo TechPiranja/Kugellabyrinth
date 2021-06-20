7826fc390a18fb0e8aaabd317eaca9de01766848
Piranja <techpiranja@gmail.com>
2021-06-20 09:24:14 +0200

    improved cell styling

--- app/src/main/java/com/example/kugellabyrinth/ScoreAdapter.java
+++ app/src/main/java/com/example/kugellabyrinth/ScoreAdapter.java
@@ -15,9 +15,6 @@ import java.util.List;
 
 
 public class ScoreAdapter extends ArrayAdapter<Score> {
-
-    private static final DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyy HH:mm:ss");
-
     public ScoreAdapter(@NonNull Context context, List<Score> scores) {
         super(context, 0, scores);
     }
@@ -28,20 +25,21 @@ public class ScoreAdapter extends ArrayAdapter<Score> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.score_cell, parent, false);
 
         TextView username = convertView.findViewById(R.id.cellUsername);
-        TextView level = convertView.findViewById(R.id.cellLevel);
         TextView timeSpent = convertView.findViewById(R.id.cellTimeSpent);
+        TextView winnerPlace = convertView.findViewById(R.id.winnerPlace);
 
         assert score != null;
         username.setText(score.getUsername());
-        level.setText(String.valueOf(score.getLevel()));
 
-        int seconds = (int) (score.getTimeSpent() / 1000);
+        int seconds = score.getTimeSpent() / 1000;
         int minutes = seconds / 60;
         seconds = seconds % 60;
         int millis = score.getTimeSpent() % 1000;
         String timeSpentText = String.format("%d m %02d s %03d ms", minutes, seconds, millis);
         timeSpent.setText(timeSpentText);
-
+        System.out.println(position);
+        // we have to add 1, so the list starts at 1 ant not 0
+        winnerPlace.setText(String.valueOf(position + 1));
         return convertView;
     }
 }

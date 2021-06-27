372311f23de2e7781aac495090b8f2669b66f5a6
Piranja <techpiranja@gmail.com>
2021-06-27 15:54:45 +0200

    added missing javadocs

--- app/src/main/java/com/example/kugellabyrinth/EventListener.java
+++ app/src/main/java/com/example/kugellabyrinth/EventListener.java
@@ -1,5 +1,13 @@
 package com.example.kugellabyrinth;
 
+/**
+ * The interface Event listener.
+ */
 public interface EventListener {
+    /**
+     * Send data to activity.
+     *
+     * @param data the data
+     */
     public void sendDataToActivity(String data);
 }
--- app/src/main/java/com/example/kugellabyrinth/Player.java
+++ app/src/main/java/com/example/kugellabyrinth/Player.java
@@ -1,6 +1,15 @@
 package com.example.kugellabyrinth;
 
+/**
+ * The type Player.
+ */
 public class Player {
+    /**
+     * The X.
+     */
     public int x = 1;
+    /**
+     * The Y.
+     */
     public int y = 0;
 }
